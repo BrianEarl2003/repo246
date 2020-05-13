717de2cb7d0fd9033e37c8e5db65613822a19851
Brian Earl <ear18009@byui.edu>
2020-05-13 10:04:59 -0600

    I do what I want

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -3,7 +3,7 @@ package prove04;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Brian Earl");
+	    System.out.println("No, my name is Brian Earl");
         System.out.println("Hello Brian, my name is Briana");
     }
 }
