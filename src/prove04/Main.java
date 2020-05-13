e9b2d2f9cd9993bd3a35a9a7406289badf103b41
Brian Earl <ear18009@byui.edu>
2020-05-13 12:04:16 -0600

    printed introductory phrase

--- src/prove04/Main.java
+++
@@ -1,9 +0,0 @@
-package prove04;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("No, my name is Brian Earl");
-        System.out.println("Hello Brian, my name is Briana");
-    }
-}
